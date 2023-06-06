f4a2fe07982d151d955b83d3910ee3df11f23c1e
syusei <luckysyusei@gmail.com>
2023-06-06 21:47:08 +0900

    集成热部署

--- src/main/java/com/luckusyusei/wiki/controller/TestController.java
+++ src/main/java/com/luckusyusei/wiki/controller/TestController.java
@@ -1,17 +1,22 @@
 package com.luckusyusei.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController//返回字符串 @Controller 返回hello页面（前后端分离用不到）
 public class TestController {
+
+    @Value("${test.hello:Test}")
+    private String testHello;
+
     @GetMapping("/hello")//新增一个接口
     public String hello(){
-        return "hello world";
+        return "hello,world" + testHello;
     }
     @PostMapping("/hello/post")//新增一个接口
     public String helloPost(String name){
-        return "hello world!Post," + name;
+        return "hello,world,Post," + name;
     }
 }
