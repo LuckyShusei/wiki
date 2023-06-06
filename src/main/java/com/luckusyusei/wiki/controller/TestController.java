df65b944de40325c579f6fd2fef92acad1027ed7
syusei <luckysyusei@gmail.com>
2023-06-06 20:42:23 +0900

    增加hello接口测试test.http

--- src/main/java/com/luckusyusei/wiki/controller/TestController.java
+++ src/main/java/com/luckusyusei/wiki/controller/TestController.java
@@ -1,12 +1,17 @@
 package com.luckusyusei.wiki.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController//返回字符串 @Controller 返回hello页面（前后端分离用不到）
 public class TestController {
-    @RequestMapping("/hello")//新增一个接口
+    @GetMapping("/hello")//新增一个接口
     public String hello(){
         return "hello world";
+    }
+    @PostMapping("/hello/post")//新增一个接口
+    public String helloPost(String name){
+        return "hello world!Post," + name;
     }
 }
