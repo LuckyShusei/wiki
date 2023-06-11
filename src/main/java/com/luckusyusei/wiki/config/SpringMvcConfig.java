a690c4251a0641d93d43838f85af887704ba9443
syusei <luckysyusei@gmail.com>
2023-06-12 01:08:59 +0900

    增加AOP，打印接口耗时，请求参数和访问参数 去掉过滤器和拦截器

--- src/main/java/com/luckusyusei/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/luckusyusei/wiki/config/SpringMvcConfig.java
@@ -1,21 +1,21 @@
-package com.luckusyusei.wiki.config;
-
-import com.luckusyusei.wiki.interceptor.LogInterceptor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.annotation.Resource;
-
-@Configuration
-public class SpringMvcConfig implements WebMvcConfigurer {
-
-    @Resource
-    LogInterceptor loginInterceptor;
-
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(loginInterceptor)
-                .addPathPatterns("/**").excludePathPatterns("/login");
-
-    }
-}
+//package com.luckusyusei.wiki.config;
+//
+//import com.luckusyusei.wiki.interceptor.LogInterceptor;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import javax.annotation.Resource;
+//
+//@Configuration
+//public class SpringMvcConfig implements WebMvcConfigurer {
+//
+//    @Resource
+//    LogInterceptor loginInterceptor;
+//
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(loginInterceptor)
+//                .addPathPatterns("/**").excludePathPatterns("/login");
+//
+//    }
+//}
