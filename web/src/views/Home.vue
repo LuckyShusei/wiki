f68933a44cce55e76167330fc3810e6fbd988f5c
syusei <luckysyusei@gmail.com>
2023-06-11 20:26:12 +0900

    集成图标组件 npm install @ant-design/icons-vue --save

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -2,9 +2,9 @@
   <a-layout>
     <a-layout-content style="padding: 0 50px">
       <a-breadcrumb style="margin: 16px 0">
-<!--        <a-breadcrumb-item>Home</a-breadcrumb-item>-->
-<!--        <a-breadcrumb-item>List</a-breadcrumb-item>-->
-<!--        <a-breadcrumb-item>App</a-breadcrumb-item>-->
+        <a-breadcrumb-item>Home</a-breadcrumb-item>
+        <a-breadcrumb-item>List</a-breadcrumb-item>
+        <a-breadcrumb-item>App</a-breadcrumb-item>
       </a-breadcrumb>
       <a-layout style="padding: 24px 0; background: #fff">
         <a-layout-sider width="200" style="background: #fff">
@@ -53,7 +53,41 @@
           </a-menu>
         </a-layout-sider>
         <a-layout-content :style="{ padding: '0 24px', minHeight: '280px' }">
-          Content
+          <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
+            <template #footer>
+              <div>
+                <b>ant design vue</b>
+                footer part
+              </div>
+            </template>
+            <template #renderItem="{ item }">
+              <a-list-item key="item.title">
+                <template #actions>
+          <span v-for="{ type, text } in actions" :key="type">
+            <component v-bind:is="type" style="margin-right: 8px"/>
+            {{ text }}
+          </span>
+                </template>
+                <template #extra>
+                  <img
+                      width="272"
+                      alt="logo"
+                      src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+                  />
+                </template>
+                <a-list-item-meta :description="item.description">
+                  <template #title>
+                    <a :href="item.href">{{ item.title }}</a>
+                  </template>
+                  <template #avatar>
+                    <a-avatar :src="item.avatar"/>
+                  </template>
+                </a-list-item-meta>
+                {{ item.content }}
+              </a-list-item>
+            </template>
+          </a-list>
+
         </a-layout-content>
       </a-layout>
     </a-layout-content>
@@ -61,15 +95,55 @@
 </template>
 
 <script lang="ts">
-import {defineComponent} from 'vue';
+import {defineComponent, onMounted, ref, reactive, toRef} from 'vue';
 import axios from 'axios';
+
+const listData: Record<string, string>[] = [];
+
+for (let i = 0; i < 23; i++) {
+  listData.push({
+    href: 'https://www.antdv.com/',
+    title: `ant design vue part ${i}`,
+    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+    description:
+        'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+    content:
+        'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+  });
+}
 
 export default defineComponent({
   name: 'Home',
   setup() {
     console.log("setup");
-    axios.get("http://localhost:8880/ebook/list?name=Spring").then((response) => {
-    })
+    const ebooks = ref();
+    const ebooks1 = reactive({books: []});
+
+
+    onMounted(() => {
+      console.log("onMounted")
+      axios.get("http://localhost:8880/ebook/list?name=Spring").then((response) => {
+        const data = response.data;
+        ebooks.value = data.content;
+        ebooks1.books = data.content;
+      });
+    });
+    return {
+      ebooks,
+      books2: toRef(ebooks1, "books"),
+      listData,
+      pagination: {
+        onChange: (page: number) => {
+          console.log(page);
+        },
+        pageSize: 3,
+      },
+      actions: [
+        {type: 'StarOutlined', text: '156'},
+        {type: 'LikeOutlined', text: '156'},
+        {type: 'MessageOutlined', text: '2'},
+      ],
+    }
   }
 });
 </script>
