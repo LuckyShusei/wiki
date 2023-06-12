9bdee61ac87dfc34588438d7a68aef54124b9863
syusei <luckysyusei@gmail.com>
2023-06-12 19:41:55 +0900

    修改电子书管理页面布局

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -1,5 +1,20 @@
 <template>
-  <div class="about">
-    <h1>Ebook Manage</h1>
-  </div>
+  <a-layout>
+    <a-layout-content style="padding: 0 50px">
+      <a-breadcrumb style="margin: 16px 0">
+        <a-breadcrumb-item>Home</a-breadcrumb-item>
+        <a-breadcrumb-item>List</a-breadcrumb-item>
+        <a-breadcrumb-item>App</a-breadcrumb-item>
+      </a-breadcrumb>
+      <a-layout style="padding: 24px 0; background: #fff">
+        <a-layout-content :style="{ padding: '0 24px', minHeight: '280px' }"
+        >
+          <div class="about">
+            <h1>Ebook Manage</h1>
+          </div>
+
+        </a-layout-content>
+      </a-layout>
+    </a-layout-content>
+  </a-layout>
 </template>
