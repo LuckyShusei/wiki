cf0ccd4b13455fc04c38c4313946e723912c4bd9
syusei <luckysyusei@gmail.com>
2023-06-13 20:18:03 +0900

    首页显示电子书列表，使用固定分页参数，一次性查出所有的电子书

--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -83,19 +83,19 @@
 import {defineComponent, onMounted, ref, reactive, toRef} from 'vue';
 import axios from 'axios';
 
-const listData: Record<string, string>[] = [];
+// const listData: Record<string, string>[] = [];
 
-for (let i = 0; i < 23; i++) {
-  listData.push({
-    href: 'https://www.antdv.com/',
-    title: `ant design vue part ${i}`,
-    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-    description:
-        'Ant Design, a design language for background applications, is refined by Ant UED Team.',
-    content:
-        'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
-  });
-}
+// for (let i = 0; i < 23; i++) {
+  // listData.push({
+//     href: 'https://www.antdv.com/',
+//     title: `ant design vue part ${i}`,
+//     avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+//     description:
+//         'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+//     content:
+//         'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+//   });
+// }
 
 export default defineComponent({
   name: 'Home',
@@ -106,17 +106,21 @@ export default defineComponent({
 
 
     onMounted(() => {
-      console.log("onMounted7875")
-      axios.get("/ebook/list").then((response) => {
+      axios.get("/ebook/list",{
+        params:{
+        page:1,
+          size:1000
+      }
+      }).then((response) => {
         const data = response.data;
-        ebooks.value = data.content;
-        ebooks1.books = data.content;
-      });
+        ebooks.value = data.content.list;
+      })
     });
+
     return {
       ebooks,
-      books2: toRef(ebooks1, "books"),
-      listData,
+      // books2: toRef(ebooks1, "books"),
+      // listData,
       pagination: {
         onChange: (page: number) => {
           console.log(page);
@@ -132,6 +136,7 @@ export default defineComponent({
   }
 });
 </script>
+
 <style scoped>
 .ant-avatar {
   width: 50px;
