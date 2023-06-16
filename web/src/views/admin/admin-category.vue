153127195571e49d72d1416ce60614d8df756c64
syusei <luckysyusei@gmail.com>
2023-06-16 20:46:33 +0900

    分离表格按树形结构展示 增加数组转树形数据工具方法

--- web/src/views/admin/admin-category.vue
+++ web/src/views/admin/admin-category.vue
@@ -24,7 +24,7 @@
       <a-table
           :columns="columns"
           :row-key="record => record.id"
-          :data-source="categorys"
+          :data-source="level1"
           :loading="loading"
           :pagination="false"
       >
@@ -108,6 +108,19 @@ export default defineComponent({
         slots: {customRender: 'action'}
       }
     ];
+    /**
+     * 一级分类树，children属性就是二级分类
+     * [{
+     *   id: "",
+     *   name: "",
+     *   children: [{
+     *     id: "",
+     *     name: "",
+     *   }]
+     * }]
+     */
+    const level1 = ref(); // 一级分类树，children属性就是二级分类
+    // level1.value = [];
 
     /**
      * 数据查询
@@ -120,7 +133,11 @@ export default defineComponent({
         const data = response.data;
         if(data.success){
         categorys.value = data.content;
-        // 重置分页按钮
+          console.log("原始数组：", categorys.value);
+
+          level1.value = [];
+          level1.value = Tool.array2Tree(categorys.value, 0);
+          console.log("树形结构：", level1);
         }else{
           message.error(data.message);
         }
@@ -179,7 +196,8 @@ export default defineComponent({
 
     return {
       param,
-      categorys,
+      // categorys,
+      level1,
       columns,
       loading,
       handleQuery,
