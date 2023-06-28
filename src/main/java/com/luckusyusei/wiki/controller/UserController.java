5648f14d43edd2e1382d521dc4dc5f0c5b25832e
syusei <luckysyusei@gmail.com>
2023-06-28 19:01:45 +0900

    集成redis，将登陆成功信息放入redis中

--- src/main/java/com/luckusyusei/wiki/controller/UserController.java
+++ src/main/java/com/luckusyusei/wiki/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.luckusyusei.wiki.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.luckusyusei.wiki.Response.CommonResp;
 import com.luckusyusei.wiki.Response.PageResp;
 import com.luckusyusei.wiki.Response.UserLoginResp;
@@ -7,6 +8,11 @@ import com.luckusyusei.wiki.Response.UserQueryResp;
 import com.luckusyusei.wiki.req.UserLoginReq;
 import com.luckusyusei.wiki.req.UserQueryReq;
 import com.luckusyusei.wiki.req.UserResetPasswordReq;
+import com.luckusyusei.wiki.service.EbookService;
+import com.luckusyusei.wiki.util.SnowFlake;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.luckusyusei.wiki.req.UserSaveReq;
 import com.luckusyusei.wiki.service.UserService;
 import org.springframework.util.DigestUtils;
@@ -14,49 +20,70 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
     @Resource
     private UserService userService;
+    @Resource
+    private SnowFlake snowFlake;
+    @Resource
+    private RedisTemplate redisTemplate;
 
     @GetMapping("/list")//新增一个接口
-    public CommonResp list(@Valid UserQueryReq req){
+    public CommonResp list(@Valid UserQueryReq req) {
         CommonResp<PageResp<UserQueryResp>> resp = new CommonResp<>();
-        PageResp<UserQueryResp> list =userService.list(req);
+        PageResp<UserQueryResp> list = userService.list(req);
         resp.setContent(list);
         return resp;
     }
+
     @PostMapping("/save")//新增一个save接口
-    public CommonResp save(@Valid @RequestBody UserSaveReq req){
+    public CommonResp save(@Valid @RequestBody UserSaveReq req) {
         req.setPassword(DigestUtils.md5DigestAsHex(req.getPassword().getBytes()));
         CommonResp resp = new CommonResp<>();
         userService.save(req);
         return resp;
     }
-    @DeleteMapping  ("/delete/{id}")//新增一个delete接口
-    public CommonResp delete(@PathVariable Long id){
+
+    @DeleteMapping("/delete/{id}")//新增一个delete接口
+    public CommonResp delete(@PathVariable Long id) {
         CommonResp resp = new CommonResp<>();
         userService.delete(id);
         return resp;
     }
+
     @PostMapping("/reset-password")
     public CommonResp resetPassword(@Valid @RequestBody UserResetPasswordReq req) {
         req.setPassword(DigestUtils.md5DigestAsHex(req.getPassword().getBytes()));
         CommonResp resp = new CommonResp<>();
         userService.resetPassword(req);
         return resp;
-
     }
+
     @PostMapping("/login")
     public CommonResp login(@Valid @RequestBody UserLoginReq req) {
         req.setPassword(DigestUtils.md5DigestAsHex(req.getPassword().getBytes()));
         CommonResp<UserLoginResp> resp = new CommonResp<>();
         UserLoginResp userLoginResp = userService.login(req);
 
+        Long token = snowFlake.nextId();
+        LOG.info("生成单点登录token：{}，并放入redis中", token);
+        userLoginResp.setToken(token.toString());
+        redisTemplate.opsForValue().set(token.toString(), JSONObject.toJSONString(userLoginResp), 3600 * 24, TimeUnit.SECONDS);
+
         resp.setContent(userLoginResp);
         return resp;
     }
 
+    @GetMapping("/logout/{token}")
+    public CommonResp logout(@PathVariable String token) {
+        CommonResp resp = new CommonResp<>();
+        redisTemplate.delete(token);
+        LOG.info("从redis中删除token: {}", token);
+        return resp;
+    }
 }
